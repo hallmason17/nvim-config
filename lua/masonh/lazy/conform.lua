a74ddbdb0fefb54e5da945978262ab42790251a8
Mason Hall <hallmason17@gmail.com>
2024-08-18 20:57:45 -0400

    formatting

--- lua/masonh/lazy/conform.lua
+++ lua/masonh/lazy/conform.lua
@@ -1,24 +1,26 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require('conform')
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
 
-        conform.setup({
-            formatters_by_ft = {
-                go = { "gofumpt", "goimports" },
-                lua = { "stylua" },
-                c = { "clang-format", "clangd" },
-                python = { "black" },
-                elixir = { "mix" },
-                rust = { "rust-analyzer" },
-                typescript = { "prettierd" },
-            },
-            format_on_save = {
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            }
-        })
-    end,
+		conform.setup({
+			formatters_by_ft = {
+				go = { "gofumpt", "goimports" },
+				lua = { "stylua" },
+				c = { "clang-format", "clangd" },
+				python = { "black" },
+				elixir = { "mix" },
+				haskell = { "ormolu" },
+				rust = { "rust-analyzer" },
+				typescript = { "prettierd" },
+				ocaml = { "ocamlformat " },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+	end,
 }
--- lua/masonh/lazy/linting.lua
+++ lua/masonh/lazy/linting.lua
@@ -1,16 +1,17 @@
 return {
 
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-        lint.linters_by_ft = {
-            c = { "cspell", "cpplint" },
-            cpp = { "cspell", "cpplint" },
-            go = { "golangci-lint" },
-            typescript = { "eslint_d" },
-            python = { "ruff", "mypy" },
-        }
-    end,
+		lint.linters_by_ft = {
+			c = { "cspell", "cpplint" },
+			cpp = { "cspell", "cpplint" },
+			go = { "golangci-lint" },
+			haskell = { "hlint" },
+			typescript = { "eslint_d" },
+			python = { "ruff", "mypy" },
+		}
+	end,
 }
